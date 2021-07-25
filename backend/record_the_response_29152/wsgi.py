26bc5b11ef2fd41403f1d2f79470f8132db0d657
Crowdbotics <team@crowdbotics.com>
2021-07-25 15:03:51 +0000

    Update models

--- backend/record_the_response_29152/wsgi.py
+++ backend/record_the_response_29152/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'record_the_response_29152.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "record_the_response_29152.settings")
 
 application = get_wsgi_application()
